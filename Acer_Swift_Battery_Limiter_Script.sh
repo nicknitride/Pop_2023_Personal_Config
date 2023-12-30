b6ee493de1d1e65fc784d8d14de7be2d8b1a3d90
NickNitride <nick123abl@gmail.com>
2023-12-30 13:15:50 +0800

    Added git for battery

--- Acer_Swift_Battery_Limiter_Script.sh
+++ Acer_Swift_Battery_Limiter_Script.sh
@@ -4,3 +4,4 @@ sudo apt install build-essential linux-headers-$(uname -r) git
 git clone https://github.com/frederik-h/acer-wmi-battery.git
 cd acer-wmi-battery
 make
+sudo insmod acer-wmi-battery.ko
