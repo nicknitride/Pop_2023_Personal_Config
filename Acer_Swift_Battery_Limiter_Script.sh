9e2b0e9b13c7631d792668cc713dba770a9422ee
NickNitride <nick123abl@gmail.com>
2023-12-30 13:12:59 +0800

    Added laptop battery config

---
+++ Acer_Swift_Battery_Limiter_Script.sh
@@ -0,0 +1,6 @@
+sudo apt install dwarves
+sudo cp /sys/kernel/btf/vmlinux /usr/lib/modules/$(uname -r)/build/
+sudo apt install build-essential linux-headers-$(uname -r) git
+git clone https://github.com/frederik-h/acer-wmi-battery.git
+cd acer-wmi-battery
+make
