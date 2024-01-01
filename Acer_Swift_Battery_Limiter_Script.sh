2ba2e48b876f921a9227505245f0256cc88cfca5
NickNitride <nick123abl@gmail.com>
2024-01-01 15:37:47 +0800

    Added permanent install code to script

--- Acer_Swift_Battery_Limiter_Script.sh
+++ Acer_Swift_Battery_Limiter_Script.sh
@@ -4,4 +4,10 @@ sudo apt install build-essential linux-headers-$(uname -r) git
 git clone https://github.com/frederik-h/acer-wmi-battery.git
 cd acer-wmi-battery
 make
-sudo insmod acer-wmi-battery.ko
+sudo cp acer-wmi-battery.ko /lib/modules/$(uname -r)/kernel/drivers
+sudo depmod -a
+sudo -i 
+sudo echo "acer-wmi-battery" >> /etc/modules
+sudo update-initramfs -u
+sudo reboot
+
